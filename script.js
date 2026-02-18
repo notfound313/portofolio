0529478982b20921187eec2d1fdbfc8534b16043
notfound313 <marufmuh666@gmail.com>
2026-02-18 16:00:16 +0700

    change style

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-let currentLang = "en";
+let currentLang = "id"; // DEFAULT INDONESIA
 
 function toggleLanguage() {
-  currentLang = currentLang === "en" ? "id" : "en";
-
+  currentLang = currentLang === "id" ? "en" : "id";
   document.querySelectorAll("[data-en]").forEach(el => {
     el.textContent = el.dataset[currentLang];
   });
+}
+
+function toggleTheme() {
+  const html = document.documentElement;
+  html.dataset.theme = html.dataset.theme === "dark" ? "light" : "dark";
+}
+
+// MODAL
+function openModal(src) {
+  document.getElementById("modal").style.display = "block";
+  document.getElementById("modal-img").src = src;
+}
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
 }
